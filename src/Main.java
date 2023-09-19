3e0f2e303a87bd4e8b47d57a2f2522f2a4f847d4
Artur Vakhitov <arthurvakhitov@gmail.com>
2023-09-19 15:34:38 +0300

    Done task course 1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-//        Scanner consoleInput = new Scanner(System.in);
-//        String text = consoleInput.nextLine();
-//        int textLength = text.length();
-//        if (textLength % 2 == 0) {
-//            System.out.println(text.substring(0, (textLength / 2)));
-//            System.out.println(text.substring((textLength / 2), textLength));
-//        }
-//        else {
-//            System.out.println(text.substring(0, (textLength / 2) + 1));
-//            System.out.println(text.substring(((textLength / 2) + 1), textLength));
-//        }
+        Scanner consoleInput = new Scanner(System.in);
+        String text = consoleInput.nextLine();
+        int textLength = text.length();
+        if (textLength % 2 == 0) {
+            System.out.println(text.substring(0, (textLength / 2)));
+            System.out.println(text.substring((textLength / 2), textLength));
+        }
+        else {
+            System.out.println(text.substring(0, (textLength / 2) + 1));
+            System.out.println(text.substring(((textLength / 2) + 1), textLength));
+        }
     }
 }
